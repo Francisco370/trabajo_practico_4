065c8eeab9db3227341f24f4c4994324af73f811
PC <PC@DESKTOP-TQFA51R>
2024-05-20 20:58:35 -0300

    Se crearon los paquetes y clases correspondientes

---
+++ tp4_SanchezFrancisco/src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.model;
+
+import java.time.LocalDate;
+
+public class Alumno {
+	private int dni;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private int teléfono;
+	private LocalDate fechaNacimiento;
+	private String domicilio;
+	private int lu;
+}
---
+++ tp4_SanchezFrancisco/src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.model;
+
+public class Materia {
+	private String codigo;
+	private String nombre;
+	private String curso;
+	private int cantidadHoras;//
+	private String modalidad;
+	private String docente;
+	private String Carrera;
+}
