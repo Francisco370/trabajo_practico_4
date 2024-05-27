03f1a6761612afcde5ff34e4caab10777d6c9879
PC <PC@DESKTOP-TQFA51R>
2024-05-27 17:48:42 -0300

    Se completaron las clases correspondientes

--- tp4_SanchezFrancisco/src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ tp4_SanchezFrancisco/src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -3,6 +3,50 @@ package ar.edu.unju.fi.model;
 public class Carrera {
 	private int codigo;
 	private String nombre;
-	private int cantAños;
+	private int cantAnios;
 	private String estado;
+	
+	public Carrera(){
+		
+	}
+
+	public Carrera(int codigo, String nombre, int cantAnios, String estado) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.cantAnios = cantAnios;
+		this.estado = estado;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCantAnios() {
+		return cantAnios;
+	}
+
+	public void setCantAnios(int cantAnios) {
+		this.cantAnios = cantAnios;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
 }
