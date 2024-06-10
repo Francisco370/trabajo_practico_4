d80b7765fbdc5921c8df2c078a760008c81e98ff
PC <PC@DESKTOP-TQFA51R>
2024-06-10 18:10:57 -0300

    Todo lo correspondiente a carrera, finalizado

--- tp4_SanchezFrancisco/src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ tp4_SanchezFrancisco/src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -1,28 +1,31 @@
 package ar.edu.unju.fi.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Carrera {
-	private int codigo;
+	private int codCarrera;
 	private String nombre;
 	private int cantAnios;
-	private String estado;
+	private boolean estado;
 	
 	public Carrera(){
 		
 	}
 
-	public Carrera(int codigo, String nombre, int cantAnios, String estado) {
-		this.codigo = codigo;
+	public Carrera(int codCarrera, String nombre, int cantAnios, boolean estado) {
+		this.codCarrera = codCarrera;
 		this.nombre = nombre;
 		this.cantAnios = cantAnios;
 		this.estado = estado;
 	}
 
-	public int getCodigo() {
-		return codigo;
+	public int getCodCarrera() {
+		return codCarrera;
 	}
 
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
+	public void setCodCarrera(int codCarrera) {
+		this.codCarrera = codCarrera;
 	}
 
 	public String getNombre() {
@@ -41,12 +44,13 @@ public class Carrera {
 		this.cantAnios = cantAnios;
 	}
 
-	public String getEstado() {
+	public boolean isEstado() {
 		return estado;
 	}
 
-	public void setEstado(String estado) {
+	public void setEstado(boolean estado) {
 		this.estado = estado;
 	}
+	
 	
 }
